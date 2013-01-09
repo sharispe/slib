00aa69a1552cb8dfc504b3cf3d17b52b3e8c1ad0
Sébastien Harispe <harispe.sebastien@gmail.com>
2013-01-09 19:43:30 +0100

    Javadoc

---
+++ slib-sml/src/main/java/slib/sml/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * slib.sml is the package dedicated to the Semantic Measures Library (SML)
+ * More information about the SML and related projects can be found at 
+ * <a href="http://www.lgi2p.ema.fr/kid/tools/sml/">http://www.lgi2p.ema.fr/kid/tools/sml/</a>.
+ */
+package slib.sml;
