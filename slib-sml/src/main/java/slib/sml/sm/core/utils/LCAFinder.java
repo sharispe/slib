b079bb01a9f5e6e1ff6a399d5743a63913ea3da9
Sébastien Harispe <harispe.sebastien@gmail.com>
2013-02-13 18:09:04 +0100

    LCAFinder interface and implementation

---
+++ slib-sml/src/main/java/slib/sml/sm/core/utils/LCAFinder.java
@@ -0,0 +1,59 @@
+/*
+ * 
+ * Copyright or © or Copr. Ecole des Mines d'Alès (2012) 
+ * LGI2P research center
+ * This software is governed by the CeCILL  license under French law and
+ * abiding by the rules of distribution of free software.  You can  use, 
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info". 
+ * 
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability. 
+ * 
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or 
+ * data to be ensured and,  more generally, to use and operate it in the 
+ * same conditions as regards security. 
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ * 
+ */
+package slib.sml.sm.core.utils;
+
+import java.util.Set;
+import slib.sglib.model.graph.G;
+import slib.sglib.model.graph.elements.V;
+import slib.utils.ex.SLIB_Exception;
+
+/**
+ * 
+ * @author Harispe Sébastien <harispe.sebastien@gmail.com>
+ */
+public interface LCAFinder {
+    
+        /**
+     * Compute the set of Disjoint Common Ancestors of a pair of concepts
+     * The processed graph do not required to be transitively reduced
+     * @param graph the graph in which the search requires to be performed
+     * An exception is thrown if the queried vertices are not expressed in the graph
+     * @param a the first query vertex 
+     * @param b the second query vertex
+     * @return the set of vertices corresponding to the DCA of vertices a and b.
+     * An empty set is returned if no DCA are found (Which can theoretically never happen).
+     * 
+     * @throws SLIB_Exception 
+     */
+    Set<V> getLCA(G graph, V a, V b) throws SLIB_Exception;
+    
+}
---
+++ slib-sml/src/main/java/slib/sml/sm/core/utils/LCAFinderImpl.java
@@ -0,0 +1,179 @@
+/*
+ * 
+ * Copyright or © or Copr. Ecole des Mines d'Alès (2012) 
+ * LGI2P research center
+ * This software is governed by the CeCILL  license under French law and
+ * abiding by the rules of distribution of free software.  You can  use, 
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info". 
+ * 
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability. 
+ * 
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or 
+ * data to be ensured and,  more generally, to use and operate it in the 
+ * same conditions as regards security. 
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ * 
+ */
+package slib.sml.sm.core.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.openrdf.model.vocabulary.RDFS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import slib.sglib.algo.graph.extraction.rvf.RVF;
+import slib.sglib.algo.graph.extraction.rvf.RVF_TAX;
+import slib.sglib.algo.graph.traversal.classical.DFS;
+import slib.sglib.model.graph.G;
+import slib.sglib.model.graph.elements.V;
+import slib.sglib.model.graph.utils.Direction;
+import slib.sml.sm.core.engine.SM_Engine;
+import slib.utils.ex.SLIB_Ex_Critic;
+import slib.utils.ex.SLIB_Exception;
+import slib.utils.impl.SetUtils;
+
+/**
+ * Dummy implementation of the LCAFinder interface (high algorithmic complexity).
+ * @author Harispe Sébastien <harispe.sebastien@gmail.com>
+ */
+public class LCAFinderImpl implements LCAFinder {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private V getNextUnvisited(List<V> ancestorsOrdered, Map<V, Boolean> visited) {
+        for (int i = ancestorsOrdered.size() - 1; i >= 0; i--) {
+
+            if (visited.get(ancestorsOrdered.get(i)).equals(Boolean.FALSE)) {
+                return ancestorsOrdered.get(i);
+            }
+
+        }
+        return null;
+    }
+
+    private boolean containsUnvisitedVertices(Map<V, Boolean> visited) {
+        for (V v : visited.keySet()) {
+            if (visited.get(v).equals(Boolean.FALSE)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void printStackStatus(List<V> ancestorsOrdered, Map<V, Boolean> visited) {
+        for (int i = ancestorsOrdered.size() - 1; i >= 0; i--) {
+
+            System.out.println(ancestorsOrdered.get(i) +"\t" +visited.get(ancestorsOrdered.get(i)));
+
+        }
+    }
+
+
+    @Override
+    public Set<V> getLCA(G graph, V a, V b) throws SLIB_Exception {
+        
+        if(!graph.containsVertex(a)){
+            throw new SLIB_Ex_Critic("Graph "+graph.getURI()+" doesn't contain vertice "+a.getValue());
+        }
+        else if(!graph.containsVertex(b)){
+            throw new SLIB_Ex_Critic("Graph "+graph.getURI()+" doesn't contain vertice "+b.getValue());
+        }
+        
+        
+        Set<V> lca = new HashSet<V>();
+
+        SM_Engine engine = new SM_Engine(graph);
+
+        Set<V> ancA = engine.getAncestorsInc(a);
+        Set<V> ancB = engine.getAncestorsInc(b);
+        
+        // Test if a (resp. b) subsumes b (resp. a)
+        
+        if(ancA.contains(b)){
+            lca = SetUtils.buildSet(b);
+        }
+        else if(ancB.contains(a)){
+            lca = SetUtils.buildSet(a);
+        }
+        else{ // search the intersetion of the ancestors of the compared concepts
+
+        
+        Set<V> intersection = SetUtils.intersection(engine.getAncestorsInc(a), engine.getAncestorsInc(b));
+//        logger.info(union.toString());
+
+        // topological sort of the union
+        Set<V> queries = SetUtils.buildSet(a);
+
+        queries.add(b);
+        DFS dfs = new DFS(graph, queries, SetUtils.buildSet(RDFS.SUBCLASSOF), Direction.OUT);
+        List<V> to = dfs.getTraversalOrder();
+        
+        // We remove the queried vertices from the topological order
+        to.remove(a);
+        to.remove(b);
+
+//        logger.debug("Traversal Order : " + to);
+
+        List<V> ancestorsOrdered = new ArrayList<V>(to.size());
+
+        for (V v : to) {
+            if (intersection.contains(v)) {
+                ancestorsOrdered.add(v);
+            }
+        }
+
+//        logger.debug("Union size : " + union.size());
+//        logger.debug("Ancestors ordered : " + ancestorsOrdered.size());
+
+        // Create dataStructure which will help us to manage 
+        // the visited vertices
+
+        Map<V, Boolean> isVisited = new HashMap<V, Boolean>(ancestorsOrdered.size());
+        for (V v : ancestorsOrdered) {
+            isVisited.put(v, Boolean.FALSE);
+        }
+
+        RVF rvf = new RVF_TAX(graph, Direction.OUT);
+
+        // Bottom Up with removing
+        while (containsUnvisitedVertices(isVisited)) {
+            V v = getNextUnvisited(ancestorsOrdered, isVisited);
+
+//            logger.debug("Processing " + v);
+            lca.add(v);
+            isVisited.put(v, Boolean.TRUE);
+            
+            Set<V> ancestorsV = rvf.getRV(v);
+            
+            for(V anc : ancestorsV){
+                isVisited.put(anc, Boolean.TRUE);
+            }
+
+//            printStackStatus(ancestorsOrdered, isVisited);
+        }
+
+        }
+//        logger.info("Searching DCA: end");
+//        logger.info("DCA ("+dca.size()+"): "+dca);
+        return lca;
+    }
+}
