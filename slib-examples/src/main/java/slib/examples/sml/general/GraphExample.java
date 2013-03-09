494bf60adf49d25880d81ce12f771f5136d8483c
Sébastien Harispe <harispe.sebastien@gmail.com>
2013-03-09 11:10:35 +0100

    Correct comments

--- slib-examples/src/main/java/slib/examples/sml/general/GraphExample.java
+++ slib-examples/src/main/java/slib/examples/sml/general/GraphExample.java
@@ -1,34 +1,3 @@
-/*
- * 
- * Copyright or © or Copr. Ecole des Mines d'Alès (2012) 
- * LGI2P research center
- * This software is governed by the CeCILL  license under French law and
- * abiding by the rules of distribution of free software.  You can  use, 
- * modify and/ or redistribute the software under the terms of the CeCILL
- * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info". 
- * 
- * As a counterpart to the access to the source code and  rights to copy,
- * modify and redistribute granted by the license, users are provided only
- * with a limited warranty  and the software's author,  the holder of the
- * economic rights,  and the successive licensors  have only  limited
- * liability. 
- * 
- * In this respect, the user's attention is drawn to the risks associated
- * with loading,  using,  modifying and/or developing or reproducing the
- * software by the user in light of its specific status of free software,
- * that may mean  that it is complicated to manipulate,  and  that  also
- * therefore means  that it is reserved for developers  and  experienced
- * professionals having in-depth computer knowledge. Users are therefore
- * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or 
- * data to be ensured and,  more generally, to use and operate it in the 
- * same conditions as regards security. 
- * 
- * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license and that you accept its terms.
- * 
- */
 package slib.examples.sml.general;
 
 import java.util.Set;
@@ -70,7 +39,7 @@ public class GraphExample {
         
         URI relA    = factory.createURI("http://graph/interact");
         
-        // We create a graph which is load in memory
+        // We create a graph which is loaded in memory
         G graph = new GraphMemory(uriGraph);
         
         System.out.println("- Empty Graph");
@@ -122,7 +91,7 @@ public class GraphExample {
         System.out.println("Descendants vB: "+descendantsB);
         
         
-        // Retrieve the all the vertices and edges associated to a graph
+        // Retrieve all the vertices and edges associated to a graph
         
         Set<V> vertices = graph.getV();
         Set<E> edges = graph.getE();
@@ -139,7 +108,7 @@ public class GraphExample {
         
         
         System.out.println("- Graph, edges and vertices removed");
-        // Remove the vertices or the edges
+        // Remove the vertices and the edges
         graph.removeV(vertices); // associated edges are also removed
         graph.removeE(edges);
         
