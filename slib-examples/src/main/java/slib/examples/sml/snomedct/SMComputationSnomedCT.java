ce85e12f62e0a180a128923b4485b5288b802c70
Sébastien Harispe <harispe.sebastien@gmail.com>
2013-03-10 20:33:34 +0100

    Snomed-CT example

--- slib-examples/src/main/java/slib/examples/sml/snomedct/SMComputationSnomedCT.java
+++ slib-examples/src/main/java/slib/examples/sml/snomedct/SMComputationSnomedCT.java
@@ -26,33 +26,33 @@ import slib.utils.impl.Timer;
  * Example of a Semantic measure computation using the Semantic Measures
  * Library. In this snippet we estimate the similarity of two concepts expressed
  * in the SNOMED-CT semantic Graph.
- * 
+ *
  * More information at http://www.semantic-measures-library.org/
  *
- * Note that you can set the LOG level in specified in log4j.xml, 
- * e.g. in root element, change value="INFO" to value="DEBUG"
+ * Note that you can set the LOG level in specified in log4j.xml, e.g. in root
+ * element, change value="INFO" to value="DEBUG"
  *
  * @author Harispe Sébastien <harispe.sebastien@gmail.com>
  */
 public class SMComputationSnomedCT {
 
     public static void main(String[] params) throws SLIB_Exception {
-        
-        // Some variables defining the location of the file from which the Snomed-CT will be loaded
-        // The loader is suited for Snomed-CT expressed in RF2 format 
-        String DATA_DIR = "/data"; // this is the directory in which the donwloaded snomed version has been extracted
+
+        // Some variables defining the locations of the files from which the Snomed-CT will be loaded
+        // The loader we use requires Snomed-CT expressed using RF2 format. 
+        String DATA_DIR = "/data"; // this is the directory in which the downloaded snomed version has been extracted
         String SNOMEDCT_VERSION = "20120731";
         String SNOMEDCT_DIR = DATA_DIR + "/SnomedCT_Release_INT_" + SNOMEDCT_VERSION + "/RF2Release/Full/Terminology";
         String SNOMEDCT_CONCEPT = SNOMEDCT_DIR + "/sct2_Concept_Full_INT_" + SNOMEDCT_VERSION + ".txt";
         String SNOMEDCT_RELATIONSHIPS = SNOMEDCT_DIR + "/sct2_Relationship_Full_INT_" + SNOMEDCT_VERSION + ".txt";
-        
+
         // We configure a timer
         Timer t = new Timer();
         t.start();
-    
-        // We create an in-memory graph in which we will load Snomed-ct.
-        // Notice that Snomed-ct is quite large (e.g. 296433 concepts, 872318 relationships ) so you will need to 
-        // allocate extra memory to the JVM e.g -Xmx3000m
+
+        // We create an in-memory graph in which we will load Snomed-CT.
+        // Notice that Snomed-CT is quite large (e.g. version 20120731 contains 296433 concepts and872318 relationships ).
+        // You will need to allocate extra memory to the JVM e.g add -Xmx3000m parameter to allocate 3Go.
         DataFactory factory = DataFactoryMemory.getSingleton();
         URI snomedctURI = factory.createURI("http://snomedct/");
         G g = new GraphMemory(snomedctURI);
@@ -64,56 +64,57 @@ public class SMComputationSnomedCT {
         GraphLoaderGeneric.populate(conf, g);
 
         System.out.println(g.toString());
-        
+
         // We compute the similarity between the concepts 
         // associated to Heart	and Myocardium, i.e. 80891009 and 74281007 respectively
         // We first build URIs correspondind to those concepts
-        URI heartURI      = factory.createURI(snomedctURI.stringValue()+"80891009"); // i.e http://snomedct/230690007
-        URI myocardiumURI = factory.createURI(snomedctURI.stringValue()+"74281007"); 
-        
+        URI heartURI = factory.createURI(snomedctURI.stringValue() + "80891009"); // i.e http://snomedct/230690007
+        URI myocardiumURI = factory.createURI(snomedctURI.stringValue() + "74281007");
+
         // First we configure an intrincic IC 
         ICconf icConf = new IC_Conf_Topo(SMConstants.FLAG_ICI_SECO_2004);
         // Then we configure the pairwise measure to use, we here choose to use Lin formula
         SMconf smConf = new SMconf("Lin", SMConstants.FLAG_SIM_PAIRWISE_DAG_NODE_LIN_1998, icConf);
-        
+
         // We define the engine used to compute the similarity
         SM_Engine engine = new SM_Engine(g);
-        
+
         // We retrieve the vertices corresponding to the two concepts
         V strokeVertex = g.getV(heartURI);
         V myocardiumVertex = g.getV(myocardiumURI);
-        
+
         double sim = engine.computePairwiseSim(smConf, strokeVertex, myocardiumVertex);
-        System.out.println("Similarity Heart/Myocardium: "+sim);
-        
-        /* Notice that the first computation is expensive as the engine compute the IC and extra information 
-        * which are cached by the engine
-        * Let's perform 100000 random computations (we only print some results).
-        * We retrieve the set of vertices as a list
-        */
+        System.out.println("Similarity Heart/Myocardium: " + sim);
+
+        /* 
+         * Notice that the first computation is expensive as the engine compute the IC and extra information 
+         * which are cached by the engine
+         * Let's perform 100000 random computations (we only print some results).
+         * We retrieve the set of vertices as a list
+         */
         int totalComparison = 100000;
-        
+
         List<V> listVertices = new ArrayList<V>(g.getV());
         int nbConcepts = listVertices.size();
-        int id1,id2;
-        V c1,c2;
+        int id1, id2;
+        V c1, c2;
         String idC1, idC2;
         Random r = new Random();
-        
+
         for (int i = 0; i < totalComparison; i++) {
             id1 = r.nextInt(nbConcepts);
             id2 = r.nextInt(nbConcepts);
-            
+
             c1 = listVertices.get(id1);
             c2 = listVertices.get(id2);
-            
+
             sim = engine.computePairwiseSim(smConf, c1, c2);
-            
-            if( (i+1) % 1000 == 0){
+
+            if ((i + 1) % 1000 == 0) {
                 idC1 = ((URI) c1.getValue()).getLocalName();
                 idC2 = ((URI) c2.getValue()).getLocalName();
-                
-                System.out.println("Sim "+(i+1)+"/"+totalComparison+"\t"+idC1+"/"+idC2+": "+sim);
+
+                System.out.println("Sim " + (i + 1) + "/" + totalComparison + "\t" + idC1 + "/" + idC2 + ": " + sim);
             }
         }
         t.stop();
